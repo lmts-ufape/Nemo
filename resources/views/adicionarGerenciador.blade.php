45ebda3eec58f4eab533ceae25b6951dedff2804
Mateus Resende <mateus.resende.r@gmail.com>
2019-03-19 17:28:47 -0300

    Ajustes no Layout

--- resources/views/adicionarGerenciador.blade.php
+++ resources/views/adicionarGerenciador.blade.php
@@ -4,21 +4,34 @@
 	<a href="/listar/pisciculturas">Pisciculturas</a> > <a href="/info/piscicultura/{{$piscicultura->id}}"> {{$piscicultura->nome}} </a> > Adicionar Gerenciador
 @stop
 @section('content')
-@if (isset($errors) && count($errors) > 0)
-<div class="alert alert-danger" role="alert">
-	@foreach($errors->getMessages() as &$error)
-		{{$error[0]}}
-	@endforeach
-</div>
-@endif
-<form action="/inserirGerenciador" method="post">
-  {{ csrf_field() }}
-  <input type="hidden" name="is_adiministrador" value=0>
-  <input type="hidden" name="piscicultura_id" value="{{$piscicultura->id}}">
-  <div class="form-group">
-    <label>Endereço de e-mail</label>
-    <input type="email" class="form-control" name="email" placeholder="exemplo@exemplo.com" value="{{old('email')}}" required>
+<div class="container">
+  <div class="row justify-content-center">
+      <div class="col-md-8">
+          <div class="card">
+              <div class="card-header">
+                  Nova Piscicultura
+              </div>
+              <div class="card-body">
+                @if (isset($errors) && count($errors) > 0)
+                <div class="alert alert-danger" role="alert">
+                  @foreach($errors->getMessages() as &$error)
+                    {{$error[0]}}
+                  @endforeach
+                </div>
+                @endif
+                <form action="/inserirGerenciador" method="post">
+                  {{ csrf_field() }}
+                  <input type="hidden" name="is_adiministrador" value=0>
+                  <input type="hidden" name="piscicultura_id" value="{{$piscicultura->id}}">
+                  <div class="form-group">
+                    <label>Endereço de e-mail</label>
+                    <input type="email" class="form-control" name="email" placeholder="exemplo@exemplo.com" value="{{old('email')}}" required>
+                  </div>
+                  <button type="submit" class="btn btn-primary">Adicionar</button>
+                </form>
+              </div>
+          </div>
+      </div>
   </div>
-  <button type="submit" class="btn btn-primary">Adicionar</button>
-</form>
+</div>
 @stop
--- resources/views/editarPiscicultura.blade.php
+++ resources/views/editarPiscicultura.blade.php
@@ -5,21 +5,36 @@
 <a href="/listar/pisciculturas">Pisciculturas</a> > <a href="/info/piscicultura/{{$piscicultura->id}}"> {{$piscicultura->nome}} </a> > Editar Piscicultura	
 @stop
 @section('content')
-@if (isset($errors) && count($errors) > 0)
-<div class="alert alert-danger" role="alert">
-	@foreach($errors->getMessages() as &$error)
-		{{$error[0]}}
-	@endforeach
-</div>
-@endif
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    Editar Piscicultura
+				</div>
+				<div class="card-body">
+					@if (isset($errors) && count($errors) > 0)
+					<div class="alert alert-danger" role="alert">
+						@foreach($errors->getMessages() as &$error)
+							{{$error[0]}}
+						@endforeach
+					</div>
+					@endif
 
-<form action="/salvarPiscicultura" method="POST">
-	{{ csrf_field() }}
-	<input type="hidden" name="id" value="{{$piscicultura->id}}"/>
-	<div class="form-group">
-		<label>Nome da Piscultura</label><br>	
-		<input type="form-control" name="nome" value="{{$piscicultura->nome}}" />
+					<form action="/salvarPiscicultura" method="POST">
+						{{ csrf_field() }}
+						<input type="hidden" name="id" value="{{$piscicultura->id}}"/>
+						<div class="form-group">
+							<label>Nome da Piscultura</label><br>	
+							<input type="form-control" name="nome" value="{{$piscicultura->nome}}" />
+						</div>
+						<input class="btn btn-primary" type="submit" value="Alterar" />		
+					</form>
+				</div>
+			</div>
+		</div>
 	</div>
-	<input class="btn btn-primary" type="submit" value="Alterar" />		
-</form>
+</div>
 @stop
+
+
